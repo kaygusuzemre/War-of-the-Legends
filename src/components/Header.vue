f0ee15b6ff9af4bdbdea75ae96c850a16d27f158
kaygusuzemre <emrekaygusuz95@gmail.com>
2022-09-29 16:38:55 +0200

    navbar dropdown edit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,6 +24,7 @@
                         <b-dropdown-item href="#">ES</b-dropdown-item>
                         <b-dropdown-item href="#">RU</b-dropdown-item>
                         <b-dropdown-item href="#">FA</b-dropdown-item>
+                        <b-dropdown-item href="#">TR</b-dropdown-item>
                     </b-nav-item-dropdown>
 
                     <b-nav-item-dropdown>
